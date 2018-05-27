206623b749ee54a2943713bbdbdaf1f353365f91
TheFirstSaiyan <19hari98@gmail.com>
2018-05-27 13:30:52 +0530

    Update sketch.js

--- karkakasadara/sketch.js
+++ karkakasadara/sketch.js
@@ -2,7 +2,8 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 	let bgpage=chrome.extension.getBackgroundPage();
 	let word=bgpage.word;
-	let url='https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180504T131049Z.ce0ad7194e99dbf1.17a1154902a55a85360d50fd4a3a93a1cf849e14&text='+word+'&lang=en-ta';
+	let api_key=' your api key from yandex API';
+	let url='https://translate.yandex.net/api/v1.5/tr.json/translate?key='+api_key+'&text='+word+'&lang=en-ta';
 	//createP(word);
 	loadJSON(url,gotData);
 	function gotData(data)
